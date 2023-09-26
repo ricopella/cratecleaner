d0bc14857d28e6a45a6fcd01ec99556a25838144
Narin Sundarabhaya <nrs710@gmail.com>
2023-09-26 23:24:14 +0700

    first commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <div className="container">
+      <h1>Coming soon</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/renderer/src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
