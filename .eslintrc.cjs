39936e5e4ed48eba21ee56e5c7dea772666e3b72
Narin Sundarabhaya <nrs710@gmail.com>
2023-10-28 14:27:12 +0700

    add eslint action and fix missing dep and configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
   ],
+  plugins: ['react-hooks', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn'
+    'react-refresh/only-export-components': 'warn',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 }
